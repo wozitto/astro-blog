a1419e65eec6a650cc96e4297bec3d2f9e227065
wozitto <hirookatoshito@gmail.com>
2024-03-16 14:16:18 +0900

    公開日と更新日はどちらも表示するように変更

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -1,4 +1,9 @@
-import { LOCALE } from "@config";
+import {
+  differenceInHours,
+  differenceInDays,
+  differenceInMonths,
+  format,
+} from "date-fns";
 
 interface DatetimesProps {
   pubDatetime: string | Date;
@@ -28,13 +33,6 @@ export default function Datetime({
         <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
         <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
       </svg>
-      {modDatetime && modDatetime > pubDatetime ? (
-        <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
-        </span>
-      ) : (
-        <span className="sr-only">Published:</span>
-      )}
       <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
         <FormattedDatetime
           pubDatetime={pubDatetime}
@@ -46,27 +44,46 @@ export default function Datetime({
 }
 
 const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
-  const myDatetime = new Date(
-    modDatetime && modDatetime > pubDatetime ? modDatetime : pubDatetime
-  );
+  const now = new Date();
+  const publishedDate = new Date(pubDatetime);
+  const modifiedDate = modDatetime ? new Date(modDatetime) : null;
 
-  const date = myDatetime.toLocaleDateString(LOCALE.langTag, {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
+  const hoursDiff = differenceInHours(now, publishedDate);
+  const daysDiff = differenceInDays(now, publishedDate);
+  const monthsDiff = differenceInMonths(now, publishedDate);
 
-  const time = myDatetime.toLocaleTimeString(LOCALE.langTag, {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  let displayDate;
+
+  if (hoursDiff < 24) {
+    displayDate = `${hoursDiff} hours ago`;
+  } else if (daysDiff < 30) {
+    displayDate = `${daysDiff} days ago`;
+  } else if (monthsDiff < 12) {
+    displayDate = `${monthsDiff} months ago`;
+  } else {
+    displayDate = format(publishedDate, "yyyy/MM/dd");
+  }
+
+  let displayModDate = "";
+  if (modifiedDate && modifiedDate > publishedDate) {
+    displayModDate = `${format(modifiedDate, "yyyy/MM/dd")}`;
+  }
 
   return (
-    <>
-      <time dateTime={myDatetime.toISOString()}>{date}</time>
-      <span aria-hidden="true"> | </span>
-      <span className="sr-only">&nbsp;at&nbsp;</span>
-      <span className="text-nowrap">{time}</span>
-    </>
+    <div>
+      Published:
+      <time dateTime={publishedDate.toISOString()} className="ml-2">
+        {displayDate}
+      </time>
+      {modifiedDate && modifiedDate > publishedDate && displayModDate && (
+        <>
+          <span aria-hidden="true"> | </span>
+          Updated:
+          <time dateTime={modifiedDate.toISOString()} className="ml-2">
+            {displayModDate}
+          </time>
+        </>
+      )}
+    </div>
   );
 };
